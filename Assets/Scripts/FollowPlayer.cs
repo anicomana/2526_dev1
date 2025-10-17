259d0071ed8be6ed1400769aae815c043e68b17d
Nicole A. Manalaysay <16nicka09@gmail.com>
2025-10-17 11:26:16 +0200

    Player Input
    Added input from user on vehicle

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,17 +2,34 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+
+    public GameObject followObject;
+    public Vector3 offset = new Vector3(0, 5, -15);
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
-        
-    }
 
-    public GameObject player;
+        if (followObject == null)
+            {
+            Debug.LogError("FollowPlayer: followObject is not assigned.");
+        }
+        else
+            {
+            Debug.Log("FollowPlayer: following " + followObject.name);
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + new Vector3(0,5,-15);
+
+    }
+
+    // LateUpdate is called once per frame after all Update functions have been called
+    void LateUpdate()
+    {
+        transform.position = followObject.transform.position + offset;
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,17 +3,28 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private float verticalSpeed = 20.0f;
+    private float turnSpeed = 50.0f;
+    private float horizontalInput;
+    private float verticalInput;
+
     void Start()
     {
-        
-    }
 
-    public float speed = 5.0f;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // Get the horizontal input
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalSpeed * verticalInput );
+
+        // Turn the vehicle 
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
